b434db27aa6578e9442d68e3059f30c3a2c5e9ca
gauravsingh2k21 <107644724+gauravsingh2k21@users.noreply.github.com>
2022-06-22 17:04:55 +0530

    Time: 22 ms (23.80%), Space: 8.1 MB (16.93%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -10,7 +10,7 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) 
     {
-        unordered_set<ListNode*> s;
+       /* unordered_set<ListNode*> s;
      while(head!=nullptr)
      {
          if(s.find(head)!= s.end())
@@ -18,6 +18,31 @@ public:
          s.insert(head);
          head=head->next;
      }
+        return false;*/
+        
+        
+        ListNode *tortoise = head;
+        if(!tortoise)
+            return false;
+        ListNode *hare = head->next;
+        
+        
+        
+        if(tortoise->next == nullptr)
+            return false;
+        
+        while(tortoise!=nullptr)
+        {
+            tortoise = tortoise->next;
+            if(hare->next == nullptr|| hare->next->next ==nullptr)
+                break;
+                
+            hare = hare->next->next;
+            
+            if(hare<=tortoise)
+                return true;
+        }
         return false;
+        
     }
 };
