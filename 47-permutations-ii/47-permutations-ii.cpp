2d955bfe73801ac79b0542160d52a9ec8193b05c
gauravsingh2k21 <107644724+gauravsingh2k21@users.noreply.github.com>
2022-07-13 20:49:35 +0530

    Time: 8 ms (80.61%), Space: 9.8 MB (41.79%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -4,32 +4,25 @@ public:
     {
         vector<vector<int>> ans;
         helper(nums,0,ans);
-        
-        set<vector<int>> s;
-        vector<vector<int>> res;
-        
-        for(auto it:ans)
-           s.insert(it); 
-        for(auto it:s)
-            res.push_back(it);
-        return res;
-        
+        return ans;
     }
     
-   void helper(vector<int>& nums,int left,vector<vector<int>> &ans)
+    void helper(vector<int>& nums,int left,vector<vector<int>> &ans)
     {
-        if(left>=nums.size())
-        {    
+        if(left==nums.size())
+        {
             ans.push_back(nums);
-            return;
-        }   
-        
-        
+           return;
+        }
+        set<int> m;
         for(int i = left;i<nums.size();++i)
         {
+            if(m.find(nums[i])!=m.end())
+               continue;
+            m.insert(nums[i]);
             swap(nums[left],nums[i]);
             helper(nums,left+1,ans);
-            swap(nums[left],nums[i]); //backtrack
+            swap(nums[left],nums[i]);
         }
     }
 };
