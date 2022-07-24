824d5dec9ff59c7b36de29d9a61fa8a78d93bcf2
gauravsingh2k21 <107644724+gauravsingh2k21@users.noreply.github.com>
2022-07-24 09:44:58 +0530

    Time: 4 ms (58.13%), Space: 12.6 MB (14.41%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -12,10 +12,8 @@
 class Solution {
 public:
     bool checkTree(TreeNode* root) 
-    {
-       
-       if(root->val == root->left->val+root->right->val)
-           return true;
-       return false; 
-    }
+    {    
+      return (root->val == root->left->val+root->right->val ? 1:0) ;
+    }    
+    
 };
