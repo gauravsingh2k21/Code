60eba94015be787552dae638b5496bf41f211cb1
gauravsingh2k21 <107644724+gauravsingh2k21@users.noreply.github.com>
2022-07-31 13:38:32 +0530

    Time: 18 ms (9.22%), Space: 7.3 MB (76.85%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPalindrome(string s) 
     {
-      for(int i=0,j=s.size()-1;i<=j;i++,j--)
+      for(int i=0,j=s.size()-1;i<j;i++,j--)
       {
           while((isalnum(s[i])==false) && i<j)
               i++;
