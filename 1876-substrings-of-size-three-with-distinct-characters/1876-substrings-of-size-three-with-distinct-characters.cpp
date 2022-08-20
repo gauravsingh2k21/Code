d40b89aa6552dbf35519342faa1043078044fa45
gauravsingh2k21 <107644724+gauravsingh2k21@users.noreply.github.com>
2022-08-20 18:16:32 +0530

    Time: 14 ms (10.15%), Space: 8.1 MB (8.29%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    int countGoodSubstrings(string s)
+    int countGoodSubstrings(string s) 
     {
       int count =0;
-      if(s.size()<3)
-          return 0;
-      for(int i=0;i<s.size()-2;++i)
+        if(s.size()<3)
+            return false ;
+      for(int i=0;i<=s.size()-3;++i)
       {
-          set<char> st;
-          
-          for(int j=i;j<i+3;++j)
+          set<int> st;
+          for(int j = i;j<i+3;j++)
           {
-              
               if(st.find(s[j])!=st.end())
-              {
-                  
                  break;
-              }    
-              st.insert(s[j]);   
-              if(j==i+2)
-                  count++;
+                  
+              st.insert(s[j]);
+              
+            if(j==i+2)
+             count++;
           }
+          
       }
-        
         return count;
     }
 };
